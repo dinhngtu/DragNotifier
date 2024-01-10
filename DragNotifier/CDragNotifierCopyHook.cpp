215065a88ea519dad72050ef0895cef88c8e4f29
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2024-01-10 13:44:53 +0100

    nanazip modifications

--- DragNotifier/CDragNotifierCopyHook.cpp
+++ DragNotifier/CDragNotifierCopyHook.cpp
@@ -1,12 +1,10 @@
 #include "pch.h"
 #include <exception>
 #include <filesystem>
-#include <fmt/core.h>
-#include <fmt/xchar.h>
 #include "CDragNotifierCopyHook.hpp"
 
-static const std::wstring DRAGNOTIFIER_PREFIX = L"{83120B49-8EFA-4CDE-8AFD-2954AE52B4A3}"s;
-static constexpr ULONG_PTR DRAGNOTIFIER_COPY = 0x83120B49;
+static const std::wstring DRAGNOTIFIER_PREFIX = L"{7F4FD2EA-8CC8-43C4-8440-CD76805B4E95}"s;
+static constexpr ULONG_PTR DRAGNOTIFIER_COPY = 0x7F4FD2EA;
 
 IFACEMETHODIMP_(UINT) CDragNotifierCopyHook::CopyCallback(
     _In_opt_ HWND hwnd,
@@ -16,16 +14,6 @@ IFACEMETHODIMP_(UINT) CDragNotifierCopyHook::CopyCallback(
     DWORD dwSrcAttribs,
     _In_opt_ PCWSTR pszDestFile,
     DWORD dwDestAttribs) {
-    auto f = fmt::format(
-        L"hwnd: {:#x}, func: {:#x}, flags: {:#x}, srcattr: {:#x}, destattr: {:#x}, src: {}, dest: {}",
-        (uintptr_t)hwnd,
-        wFunc,
-        wFlags,
-        dwSrcAttribs,
-        dwDestAttribs,
-        pszSrcFile,
-        pszDestFile);
-    OutputDebugStringW(f.c_str());
     try {
         if (wFunc == FO_COPY || wFunc == FO_MOVE) {
             std::filesystem::path srcpath(pszSrcFile);
@@ -35,8 +23,6 @@ IFACEMETHODIMP_(UINT) CDragNotifierCopyHook::CopyCallback(
                 if (hwndString.length() > 1) {
                     auto hwndDest = reinterpret_cast<HWND>(std::stoull(hwndString.substr(1)));
                     auto dest = dstpath.parent_path();
-                    f = fmt::format(L"hwnd: {:#x}, path: {}", (uintptr_t)hwndDest, dest.wstring());
-                    OutputDebugStringW(f.c_str());
                     DragNotifierCopyData data{};
                     if (SUCCEEDED(StringCchCopyW(&data.filename[0], ARRAYSIZE(data.filename), dest.c_str()))) {
                         COPYDATASTRUCT cds{ DRAGNOTIFIER_COPY, sizeof(DragNotifierCopyData), &data };
